eecd5dcfda9ae9e52cc91fe81cdae4fe9648adbd
Juraj Klucka <jurajklucka@MBPuzivlaJuraj2.home>
2024-05-26 22:01:17 +0200

    showing search results

--- src/components/JobList.tsx
+++ src/components/JobList.tsx
@@ -1,5 +1,14 @@
-export function JobList() {
-  return <ul className="job-list"></ul>;
+import JobListItem from "./JobListItem";
+
+export function JobList({ jobItems }) {
+  console.log("Joblist component rendered with jobItems", jobItems);
+  return (
+    <ul className="job-list">
+      {jobItems.map((jobItem) => (
+        <JobListItem jobItem={jobItem} />
+      ))}
+    </ul>
+  );
 }
 
 export default JobList;
