c31d3f5ffd3bbe70e57b2dc2d8379ce34373e824
Juraj Klucka <jurajklucka@MBPuzivlaJuraj2.home>
2024-05-27 20:54:50 +0200

    resturcturing app with the children pattern

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,7 +2,16 @@ import { useEffect, useState } from "react";
 import Background from "./Background";
 import Container from "./Container";
 import Footer from "./Footer";
-import Header from "./Header";
+import Header, { HeaderTop } from "./Header";
+import Logo from "./Logo";
+import BookmarksButton from "./BookmarksButton";
+import SearchForm from "./SearchForm";
+import Sidebar, { SidebarTop } from "./Sidebar";
+import JobItemContent from "./JobItemContent";
+import ResultsCount from "./ResultsCount";
+import SortingControls from "./SortingControls";
+import JobList from "./JobList";
+import PaginationControls from "./PaginationControls";
 
 function App() {
   const [jobItems, setJobItems] = useState([]);
@@ -25,8 +34,24 @@ function App() {
   return (
     <>
       <Background />
-      <Header searchText={searchText} setSearchText={setSearchText} />
-      <Container jobItems={jobItems} />
+      <Header>
+        <HeaderTop>
+          <Logo />
+          <BookmarksButton />
+        </HeaderTop>
+        <SearchForm searchText={searchText} setSearchText={setSearchText} />
+      </Header>
+      <Container>
+        <Sidebar>
+          <SidebarTop>
+            <ResultsCount />
+            <SortingControls />
+          </SidebarTop>
+          <JobList jobItems={jobItems} />
+          <PaginationControls />
+        </Sidebar>
+        <JobItemContent />
+      </Container>
       <Footer />
     </>
   );
