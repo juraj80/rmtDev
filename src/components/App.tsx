cf605809308bc19bbd70766cb4b407a1f680b448
Juraj Klucka <jurajklucka@MBPuzivlaJuraj2.home>
2024-05-28 21:42:58 +0200

    fetch jobItem custom hook

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Background from "./Background";
 import Container from "./Container";
 import Footer from "./Footer";
@@ -12,13 +12,13 @@ import ResultsCount from "./ResultsCount";
 import SortingControls from "./SortingControls";
 import JobList from "./JobList";
 import PaginationControls from "./PaginationControls";
-import { useActiveId, useJobItems } from "../lib/hooks";
+import { useActiveId, useJobItem, useJobItems } from "../lib/hooks";
 
 function App() {
   const [searchText, setSearchText] = useState("");
   const [jobItems, isLoading] = useJobItems(searchText);
-
   const activeId = useActiveId();
+  const jobItem = useJobItem(activeId);
 
   return (
     <>
@@ -40,7 +40,7 @@ function App() {
           <JobList jobItems={jobItems} isLoading={isLoading} />
           <PaginationControls />
         </Sidebar>
-        <JobItemContent />
+        <JobItemContent jobItem={jobItem} />
       </Container>
       <Footer />
     </>
--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { JobItemType } from "./types";
+import { BASE_API_URL } from "./constants";
 
 export function useActiveId() {
   const [activeId, setActiveId] = useState<number | null>(null);
@@ -20,6 +21,24 @@ export function useActiveId() {
   return activeId;
 }
 
+export function useJobItem(id: number | null) {
+  const [jobItem, setJobItem] = useState(null);
+
+  useEffect(() => {
+    if (!id) return;
+
+    const fetchData = async () => {
+      const respons = await fetch(`${BASE_API_URL}/${id}`);
+      const data = await respons.json();
+      console.log(data);
+      setJobItem(data.jobItem);
+    };
+    fetchData();
+  }, [id]);
+
+  return jobItem;
+}
+
 export function useJobItems(searchText: string) {
   const [jobItems, setJobItems] = useState<JobItemType[]>([]);
   const [isLoading, setIsLoading] = useState(false);
@@ -31,9 +50,7 @@ export function useJobItems(searchText: string) {
 
     const fetchData = async () => {
       setIsLoading(true);
-      const response = await fetch(
-        `https://bytegrad.com/course-assets/projects/rmtdev/api/data?search=${searchText}`
-      );
+      const response = await fetch(`${BASE_API_URL}?search=${searchText}`);
       const data = await response.json();
       setJobItems(data.jobItems);
       setIsLoading(false);
