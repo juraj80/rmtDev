c357de67f55bb1ea96bf839e8467561e1fee8de4
Juraj Klucka <jurajklucka@MBPuzivlaJuraj2.home>
2024-06-18 20:59:50 +0200

    extracted constant for result per page

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -14,6 +14,7 @@ import JobList from "./JobList";
 import PaginationControls from "./PaginationControls";
 import { useDebounce, useJobItems } from "../lib/hooks";
 import { Toaster } from "react-hot-toast";
+import { RESULTS_PER_PAGE } from "../lib/constants";
 
 function App() {
   //state
@@ -24,9 +25,12 @@ function App() {
 
   // derived state / computed properties
   const totalNumberOfResults = jobItems?.length || 0;
-  const totalNumberOfPages = Math.ceil(totalNumberOfResults / 7);
+  const totalNumberOfPages = Math.ceil(totalNumberOfResults / RESULTS_PER_PAGE);
   const jobItemsSliced =
-    jobItems?.slice(currentPage * 7 - 7, currentPage * 7) || [];
+    jobItems?.slice(
+      currentPage * RESULTS_PER_PAGE - RESULTS_PER_PAGE,
+      currentPage * RESULTS_PER_PAGE
+    ) || [];
 
   // event handlers / actions
   const handleChangePage = (directon: "next" | "previous") => {
